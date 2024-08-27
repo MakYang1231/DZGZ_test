862b36842904ab392dc8c233bcfc7c4aad1c7725
Mak <makyang.test@gmai.com>
2024-08-28 00:20:18 +0800

    test build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  
+  ssr: true,  // 启用服务器端渲染 (SSR)
+  target: 'static',  // 目标是静态生成
+  nitro: {
+    preset: 'node-server',  // 使用 Node.js 服务器预设，保留 API 和其他服务
+  },
+  // 如果你有 API 路由或其他自定义的服务器中间件
+  server: {
+    host: '0.0.0.0',  // 监听所有 IP 地址
+    //port: 3000,  // 指定端口
+  },
+  generate: {
+    fallback: '404.html'  // 生成静态文件时的 404 页面
+  },
+  
   app: {
     //pageTransition: { name: 'page', mode: 'out-in' },
     baseURL: process.env.NODE_ENV === 'production' ? '/DZGZ_web/' : '/DZGZ_web/',
